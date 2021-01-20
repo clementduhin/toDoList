b4cc72a4f4ee5007bb82cca9c4161bffa5b18066
Clément Duhin <c.duhin@gmail.com>
2021-01-20 15:58:25 +0100

    add form to add ToDo and list the ToDo of user connexion

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,26 +3,47 @@
 namespace App\Controller;
 
 use App\Entity\Todo;
+use App\Form\TodoType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface as ObjectManager;
+
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(Request $request): Response
+    public function index(Request $request, ObjectManager $monManager): Response
     {
 
         $repoToDo = $this->getDoctrine()->getRepository(Todo::class);
         $allToDo = $repoToDo->findAll();
         $user = $this->getUser();
-        dump($user);
+        if ($user) {
+            $toDoUser = $user->getTodos();
+        }
 
-        return $this->render('home/index.html.twig', [
-            'todos' => $allToDo,
-        ]);
+        $newToDo = new Todo;
+        $formNewTodo = $this->createForm(TodoType::class, $newToDo);
+        $formNewTodo->handleRequest($request);
+        if ($formNewTodo->isSubmitted() && $formNewTodo->isValid()) {
+            $newToDo->setCreatedAt(new \DateTime());
+            $monManager->persist($newToDo);
+            $user = $this->getUser();
+            $newToDo->addUser($user);
+            $monManager->flush();
+        }
+
+        if (isset($toDoUser)) {
+            return $this->render('home/index.html.twig', [
+                'todos' => $toDoUser,
+                'formNewTodo' => $formNewTodo->createView()
+            ]);
+        }
+
+        return $this->render('home/index.html.twig');
     }
 }
---
+++ src/Form/TodoType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Todo;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class TodoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('limitedAt', DateType::class)
+            ->add('statut', CheckboxType::class, [
+                'required' => false
+            ])
+            ->add('description', TextareaType::class)
+            ->add('name', TextType::class)
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Todo::class,
+        ]);
+    }
+}
